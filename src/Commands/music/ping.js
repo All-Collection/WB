cdd07306d7c1deeb818b9996f6603cfe77e107e1
Lasiya <lasiyadanuma531@outlook.com>
2023-04-28 23:50:26 +0530

    Update ping.js

--- src/Commands/music/ping.js
+++ src/Commands/music/ping.js
@@ -5,6 +5,7 @@ module.exports = {
     exp: 1,
     description: 'Bot response in second',
     async execute(client, arg, M) {
+        
         await M.reply(`*_${client.utils.calculatePing(M.messageTimestamp, Date.now())} second(s)_*`)
     }
 }
