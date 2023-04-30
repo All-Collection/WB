f583f1de9d59082cff72b1e6756a99a86e551c78
Lasiya <lasiyadanuma531@outlook.com>
2023-04-30 13:33:55 +0530

    Update new.js

--- src/Commands/general/new.js
+++ src/Commands/general/new.js
@@ -1,5 +1,7 @@
 const { getStats } = require('../../lib/stats')
 
+
+
 module.exports = {
     name: 'admin',
     aliases: ['ad'],
@@ -7,6 +9,10 @@ module.exports = {
     exp: 5,
     description: 'only admin',
     async execute(client, arg, M) {
+
+
+        if (!groupAdmins.includes(client.user.id.split(':')[0] + '@s.whatsapp.net') && command.category == 'moderation')
+            return M.reply('This command can only be used when bot is admin')
     
         await M.reply(
             `f`
