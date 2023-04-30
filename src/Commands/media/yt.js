85f3b3368a0a501a84f37c2728293c86058f79c9
Lasiya <lasiyadanuma531@outlook.com>
2023-04-30 17:37:00 +0530

    z

--- src/Commands/media/yt.js
+++ src/Commands/media/yt.js
@@ -8,14 +8,11 @@ module.exports = {
     exp: 5,
     description: 'Downloads given YT Link and sends  Audio/ Video',
     async execute(client, arg, M) {
-        const link = async (term) => {
-            const { videos } = await yts(term.trim())
-            if (!videos || !videos.length) return null
-            return videos[0].url
-        }
+        
+        
         if (!arg) return M.reply('Please use this command with a valid youtube.com link')
         const validPathDomains = /^https?:\/\/(youtu\.be\/|(www\.)?youtube\.com\/(embed|v|shorts)\/)/
-        const term = validPathDomains.test(arg) ? arg.trim() : await link(arg)
+        const term = validPathDomains.test(arg)
         if (!term) return M.reply('Please use this command with a valid youtube contant link')
         if (!YT.validateURL(term.trim())) return M.reply('Please use this command with a valid youtube.com link')
         const { videoDetails } = await YT.getInfo(term)
