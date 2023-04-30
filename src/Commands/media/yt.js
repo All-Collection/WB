395174b6cdfb2210b9b87e5a9425a36b2ec60830
Lasiya <lasiyadanuma531@outlook.com>
2023-04-30 17:18:39 +0530

    s

--- src/Commands/media/yt.js
+++ src/Commands/media/yt.js
@@ -2,8 +2,8 @@ const YT = require('../../lib/YT')
 const yts = require('yt-search')
 
 module.exports = {
-    name: 'lt',
-    aliases: ['bt'],
+    name: 'Youtube',
+    aliases: ['yt'],
     category: 'media',
     exp: 5,
     description: 'Downloads given YT Link and sends  Audio/ Video',
@@ -20,18 +20,19 @@ module.exports = {
         if (!YT.validateURL(term.trim())) return M.reply('Please use this command with a valid youtube.com link')
         const { videoDetails } = await YT.getInfo(term)
         
-        let text = `*Title:* ${videoDetails.title} | *Type:* Audio | *From:* ${videoDetails.ownerChannelName}`
-        client.sendMessage(
-            M.from,
-            {
-                image: {
-                    url: `https://i.ytimg.com/vi/${videoDetails.videoId}/maxresdefault.jpg`
-                },
-                caption: text
-            },
-            {
-                quoted: M
-            }
-        )
+        let text = `*${videoDetails.title}*\n${videoDetails.ownerChannelName}`
+console.log.log(videoDetails)
+     
+   
+
+
+
+
+
+
+
     }
+
+
+
 }
--- src/Commands/media/ytvideo.js
+++ src/Commands/media/ytvideo.js
@@ -19,20 +19,7 @@ module.exports = {
         if (!term) return M.reply('Please use this command with a valid youtube contant link')
         if (!YT.validateURL(term.trim())) return M.reply('Please use this command with a valid youtube.com link')
         const { videoDetails } = await YT.getInfo(term)
-        M.reply('Downloading has started please have some pesence')
-        let text = `*Title:* ${videoDetails.title} | *Type:* Video | *From:* ${videoDetails.ownerChannelName}`
-        client.sendMessage(
-            M.from,
-            {
-                image: {
-                    url: `https://i.ytimg.com/vi/${videoDetails.videoId}/maxresdefault.jpg`
-                },
-                caption: text
-            },
-            {
-                quoted: M
-            }
-        )
+       
         if (Number(videoDetails.lengthSeconds) > 1800) return M.reply('Cannot download video longer than 30 minutes')
         const audio = YT.getBuffer(term, 'video')
             .then(async (res) => {
